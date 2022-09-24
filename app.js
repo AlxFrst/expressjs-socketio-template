35988bb0a73ea0a3ffe3963e898ed5146001c0d4
AlxFrst <Alexandreforestier1107@gmail.com>
2022-09-24 01:24:43 +0100

    add db connection + db check if connected

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const io = new Server(server);
 var bodyParser = require("body-parser");
 const port = process.env.PORT || 3002;
 const clc = require("cli-color");
+require("dotenv").config();
+
+// Postgres connection
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: 5432,
+});
 
 //set view engine to ejs
 app.set("view engine", "ejs");
@@ -42,6 +53,21 @@ io.on("connection", (socket) => {
 server.listen(port, () => {
   console.log(clc.green(`Server listening on port ${port}`));
   console.log(clc.green(`http://localhost:${port}`));
+
+  // Check connection
+  pool.connect((err, client, release) => {
+    if (err) {
+      return console.error("Error acquiring client", err.stack);
+    }
+    client.query("SELECT NOW()", (err, result) => {
+      release();
+      if (err) {
+        return console.error("Error executing query", err.stack);
+      }
+      console.log(clc.blueBright("Connected database " + process.env.DB_NAME));
+    });
+  });
+
   console.log("====================================");
   console.log("Press CTRL + C to stop the server");
   console.log("====================================");
